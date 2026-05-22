b8c9ae624818c0c8a823649263d10cce716584de
jluna <jluna@example.com>
2026-05-22 14:57:14 -0500

    Fix: add extended option to express.urlencoded

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = 3000;
 
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 const upload = multer({
