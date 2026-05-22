12e1aca5ec9307ee6b3f514cee2912a869afa4d6
jluna <jluna@example.com>
2026-05-22 12:11:48 -0500

    Hardcode PORT to 3000

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
